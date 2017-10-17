54c3bfe9c5d0cfb5457026595a30344568172be8
kazik-orzechowski <kazikh2o@gmail.com>
2017-10-17 19:20:40 +0200

    Create gitignore

---
+++ src/main/java/pl/fb4j/FacebookInterfaceF4JApplication.java
@@ -0,0 +1,12 @@
+package pl.fb4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacebookInterfaceF4JApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacebookInterfaceF4JApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/fb4j/FacebookInterfaceF4JApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.fb4j;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FacebookInterfaceF4JApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
